c1b8c4152a4d41e83d5fdfc850796165e456bbb9
Mark <marco.guglielmi12@gmail.com>
2024-08-13 22:49:07 +0200

    pulizia codice

--- MyFinance/lib/Authentication/Authentication_Controller/authentication_page.dart
+++ MyFinance/lib/Authentication/Authentication_Controller/authentication_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:myfinance/Authentication/Authentication_Controller/authentication_button.dart';
+import 'package:myfinance/Authentication/Authentication_Controller/background.dart';
 
 class MyApp extends StatelessWidget {
   @override
@@ -17,14 +19,7 @@ class AuthScreen extends StatelessWidget {
     return Scaffold(
       body: Stack(
         children: [
-          Container(
-            decoration: const BoxDecoration(
-              image: DecorationImage(
-                image: AssetImage('lib/Assets/Sfondo.webp'),  //background
-                fit: BoxFit.cover,
-              )
-            ),
-          ),
+          Background(),
           Align(
             alignment: Alignment.bottomCenter,
             child: Padding(
@@ -35,39 +30,10 @@ class AuthScreen extends StatelessWidget {
                   return Column(
                     mainAxisSize: MainAxisSize.min,
                     children: [
-                      ElevatedButton(
-                        onPressed: () {
-
-                        },
-                        style: ElevatedButton.styleFrom(
-                          minimumSize: Size(buttonWidth, 50),
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(30),
-                          ),
-                          textStyle: const TextStyle(
-                            fontSize: 18,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                        child: Text('Login'),
-                      ),
-                      SizedBox(height: 10),
-                      ElevatedButton(
-                        onPressed: () {
-
-                        },
-                        style: ElevatedButton.styleFrom(
-                          minimumSize: Size(buttonWidth, 50),
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(30),
-                          ),
-                          textStyle: TextStyle(
-                            fontSize: 18,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                        child: Text('Registrati'),
-                      ),
+                      AuthenticationButton("Login", buttonWidth),
+                      const SizedBox(height: 15),
+                      AuthenticationButton("Register", buttonWidth),
+                      const SizedBox(height: 15),
                     ],
                   );
                 },
